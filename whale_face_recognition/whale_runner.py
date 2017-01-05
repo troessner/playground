eb0800db8f492ab1f5473e7df07b4185e6a3b397
Timo Rößner <timo.roessner@googlemail.com>
2017-01-05 23:11:32 +0100

    Getting the playground started.

---
+++ whale_face_recognition/whale_runner.py
@@ -0,0 +1,31 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+# dimensions of our images.
+img_width, img_height = 2500, 2500
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, input_shape=(3, img_width, img_height)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
